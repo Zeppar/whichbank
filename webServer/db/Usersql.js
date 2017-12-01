d6d173153f07d02ab413f3e236c4aa0904205009
Zeppar <849166805@qq.com>
2017-12-01 10:43:25 +0800

    add sql file

---
+++ webServer/db/Usersql.js
@@ -0,0 +1,7 @@
+var UserSQL = {
+	insert:'INSERT INTO User(userid,phone,name,idnumber,gender,birthday) VALUES (?,?,?,?,?,?)',
+	queryAll:'SELECT * FROM User',
+	getUserByPhone:'SELECT * FROM User WHERE phone = ?',
+	getUserByUserId:'SELECT * FROM User WHERE userid = ?'
+};
+module.exports = UserSQL;
