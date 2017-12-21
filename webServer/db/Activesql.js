736be541187cca275ba2f3dc616d5e8daad1d02b
Zeppar <849166805@qq.com>
2017-12-21 12:08:50 +0800

    fin active

--- webServer/db/Activesql.js
+++ webServer/db/Activesql.js
@@ -1,5 +1,7 @@
 var ActiveSQL = {	
-	deleteCode: 'DELETE FROM ACCode Where code = ?',
+	changeCodeStatus: 'UPDATE ACCode SET status=? where code=?',
 	getCode:'SELECT * FROM ACCode WHERE phone = ?',
+	createCode:'INSERT INTO ACCode (code, status) VALUES (?,?)',
+	getAllCode: 'SELECT * FROM ACCode'
 };
 module.exports = ActiveSQL;
