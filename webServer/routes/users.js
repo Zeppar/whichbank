3bb4afee5dba68fd7494acb8052bc6e8cb0401e1
Zeppar <849166805@qq.com>
2017-12-26 19:21:18 +0800

    remove Test

--- webServer/routes/users.js
+++ webServer/routes/users.js
@@ -294,8 +294,7 @@ router.post('/register', function(req, res) {
 				var time = results[0].timestamp;
 				//judge if it is out of date
 				var currentTime = new Date().getTime();
-				//				if(currentTime - time > 60 * 1000 * 5) {
-				if(0) {
+				if(currentTime - time > 60 * 1000 * 5) {
 					//delete code
 					connection.query(codeSQL.deleteCodeByPhone, [req.body.phone], function(_error2, _result2) {
 						if(error)
