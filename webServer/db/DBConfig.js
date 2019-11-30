f47a67e48075dec36f98f143ddba9dde77500dca
magene <849166805@qq.com>
2019-11-30 22:59:28 +0800

    fix bug

--- webServer/db/DBConfig.js
+++ webServer/db/DBConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
 	mysql: {
-		host: '127.0.0.1',
-		user: 'wechat_user_w',
-		password: 'MbPGuQo5HXcc',
+		host: '101.200.166.241',
+		user: 'root',
+		password: '123',
 		database: 'wechat_user',
 		port: 3306
 	}	
