cf31efe91089f71a10bd71c35ee5d89c75520b3a
magene <849166805@qq.com>
2019-11-30 22:13:09 +0800

    change mysql

--- webServer/db/DBConfig.js
+++ webServer/db/DBConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
 	mysql: {
 		host: '101.200.166.241',
-		user: 'root',
-		password: '123',
+		user: 'wechat_user_w',
+		password: 'MbPGuQo5HXcc',
 		database: 'wechat_user',
 		port: 3306
 	}	
